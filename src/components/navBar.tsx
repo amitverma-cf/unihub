c8374894cdcf1fa48b01582e0be614251de45e1e
ASWD13 <vibraniumrocker@gmail.com>
2025-01-12 17:44:18 +0530

    create post page and improved responsiveness --andy

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
-import SearchInput from './ui/SearchInput';
-import Logo from './ui/logo';
-import LoginModal from './ui/LoginModal';
+"use client";
+import React,{ useState } from "react";
+import SearchInput from "./ui/SearchInput";
+import LoginModal from "./ui/LoginModal";
+import CreateButton from "./ui/createButton";
+import Logo from "./ui/logo";
 
-
-const Header: React.FC = () => {
+function Header() {
   const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
 
   const toggleLoginModal = () => {
@@ -12,28 +13,33 @@ const Header: React.FC = () => {
   };
 
   return (
-    <header className="bg-white shadow-lg py-4 sticky top-0 z-50">
+    <header className="bg-white shadow-lg py-4 sticky top-0 z-50 ">
       <div className="container mx-auto flex items-center justify-between px-4">
-        {/* Logo*/}
-        <Logo />
+        {/* Logo */}
+        <Logo/>
 
-        {/* SearchBar Component */}
-        <SearchInput />
+        {/* div containing search bar, login, and create button */}
+        <div className="flex flex-row justify-between gap-2 items-center">
+          {/* Search Bar */}
+          <SearchInput />
 
-        {/* Login Button */}
-        <button
-          onClick={toggleLoginModal}
-          className="bg-primary hover:bg-slate-600 text-white px-4 py-2 rounded-lg transition-colors duration-300"
-        >
-          Login
-        </button>
+          {/* Login Button */}
+          <button
+            onClick={toggleLoginModal}
+            className="bg-primary hover:bg-slate-600 text-white px-4 py-2 rounded-lg transition-colors duration-300"
+          >
+            Login
+          </button>
+
+          {/* Create Post Button */}
+          <CreateButton className="rounded-lg"/>
+        </div>
       </div>
 
       {/* Render the LoginModal component */}
-      <LoginModal isOpen={isLoginModalOpen} onClose={toggleLoginModal}/>
-      
+      <LoginModal isOpen={isLoginModalOpen} onClose={toggleLoginModal} />
     </header>
   );
-};
+}
 
 export default Header;
